e4fcc109200156fbb05cbce7753f748fcbfbaa6e
yanghyeonjin <guswls7081@gmail.com>
2024-10-10 21:10:30 +0900

    config: 라이브러리 추가 (primevue, vue-router, pinia)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+        }
+    }
+});
+
+app.mount("#app");
