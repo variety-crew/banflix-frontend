9c5b29179daaa692d4ce94bce33323f74971f695
gilhan <rlfgks97@gmail.com>
2024-10-13 14:53:18 +0900

    feat: 게시판 보드 추가

---
+++ src/components/cards/preview/BoardPreviewCard.vue
@@ -0,0 +1,82 @@
+<template>
+  <Card>
+    <template #header>
+      <div class="card-header-container">
+        <div class="card-profile">
+          <Avatar :image="profileImage" class="profile-image" size="large" shape="circle" />
+          <div class="profile-nickname">{{ nickname }}</div>
+        </div>
+        <div>오후 8:33</div>
+      </div>
+    </template>
+    <template #title>{{ title }}</template>
+    <template #content>
+      <p class="m-0">{{ content }}</p>
+    </template>
+    <template #footer>
+      <div class="board-footer">
+        <div class="like">
+          <i class="pi pi-heart"></i>
+          <div class="emoji">{{ like }}</div>
+        </div>
+        <div class="comment">
+          <i class="pi pi-comment"></i>
+          <div class="emoji">{{ comment }}</div>
+        </div>
+      </div>
+    </template>
+  </Card>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const profileImage = ref('https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png');
+const nickname = ref('닉네임');
+
+const title = ref('제목');
+const content = ref('내용');
+
+const like = ref(0);
+const comment = ref(0);
+</script>
+
+<style scoped>
+.p-card {
+  /* Card */
+  padding: 20px;
+}
+
+.card-header-container {
+  display: flex;
+  justify-content: space-between;
+}
+.card-profile {
+  display: flex;
+}
+.profile-image {
+  margin-right: 20px;
+}
+.profile-nickname {
+  display: flex;
+  align-items: center;
+}
+
+.board-footer {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.like {
+  display: flex;
+  flex-direction: row;
+  margin-right: 20px;
+}
+.comment {
+  display: flex;
+  flex-direction: row;
+}
+.emoji {
+  margin-left: 5px;
+}
+</style>
--- src/views/BoardMainView.vue
+++ src/views/BoardMainView.vue
@@ -1,16 +1,35 @@
 <template>
-  <PageLayout title="커뮤니티 게시판" btn-txt="글 작성" btn-role="ALL" @click-btn="goBoardForm">
-    공지사항 페이지
+  <PageLayout title="커뮤니티" btn-txt="글 작성" btn-role="ALL" @click-btn="goBoardForm">
+    <div class="board-card-container">
+      <template v-if="cards && cards.length">
+        <template v-for="(card, index) in cards" :key="index">
+          <BoardPreviewCard :card="card" :index="index" />
+        </template>
+      </template>
+      <template v-else>게시글이 존재하지 않습니다.</template>
+    </div>
   </PageLayout>
 </template>
 
 <script setup>
-import PageLayout from '@/components/layouts/PageLayout.vue';
+import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
+import PageLayout from '@/components/layouts/PageLayout.vue';
+import BoardPreviewCard from '@/components/cards/preview/BoardPreviewCard.vue';
+
 const router = useRouter();
+
+const cards = ref([]);
+
 const goBoardForm = () => {
   router.push('/board/form');
 };
+
+onMounted(() => {
+  console.log('mounted');
+  console.log('cards.value: ', cards.value);
+  cards.value.push({ key: 'value' });
+});
 </script>
 
 <style scoped></style>
