d3dbde5c1d584749177ebf4c63106e777839ae4d
gilhan <rlfgks97@gmail.com>
2024-10-14 00:00:36 +0900

    feat: 공지사항 리스트 조회

--- src/views/NoticeMainView.vue
+++ src/views/NoticeMainView.vue
@@ -1,16 +1,40 @@
 <template>
-  <PageLayout title="공지사항" btn-txt="글 작성" btn-role="ALL" @click-btn="goNoticeForm"> 공지사항 페이지 </PageLayout>
+  <PageLayout title="공지사항" btn-txt="공지사항 등록" btn-role="ADMIN" @click-btn="goNoticeForm">
+    <template v-if="notices">
+      <template v-for="(notice, id) in notices" :key="id">
+        <NoticePreviewCard :notice="notice" class="notice-card" @click="goDetailNotice(id)"></NoticePreviewCard>
+      </template>
+    </template>
+  </PageLayout>
 </template>
 
 <script setup>
 import PageLayout from '@/components/layouts/PageLayout.vue';
+import NoticePreviewCard from '@/components/cards/preview/NoticePreviewCard.vue';
 import useToastMessage from '@/hooks/useToastMessage';
 
-const { showSuccess } = useToastMessage();
+import { ref } from 'vue';
+import router from '@/router/routes';
 
+const { showSuccess } = useToastMessage();
+const notices = ref([
+  { id: 0, title: '제목1', createdAt: '2024.10.01', writer: '홍길동 관리자' },
+  { id: 1, title: '제목2', createdAt: '2024.10.02', writer: '용길동 관리자' },
+  { id: 2, title: '제목3', createdAt: '2024.10.03', writer: '홍길할 관리자' },
+  { id: 3, title: '제목4', createdAt: '2024.10.04', writer: '홍길한 관리자' },
+  { id: 4, title: '제목5', createdAt: '2024.10.05', writer: '길동 관리자' },
+]);
+
+const goDetailNotice = id => {
+  router.push(`/notice/detail/${id}`);
+};
 const goNoticeForm = () => {
-  showSuccess('글 작성 페이지로 이동!', '이동!!');
+  router.push(`/notice/form`);
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.notice-card {
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/views/notice/NoticeDetailView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{ noticeId }}</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const noticeId = ref(route.params.noticeId);
+</script>
+
+<style scoped></style>
