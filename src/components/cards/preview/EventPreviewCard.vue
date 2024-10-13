cbec4d0b78dfa449870dbe3693927b0e70829285
gilhan <rlfgks97@gmail.com>
2024-10-14 02:19:13 +0900

    feat: 이벤트 메인페이지 추가

---
+++ src/components/cards/preview/EventPreviewCard.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>card</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- src/views/EventMainView.vue
+++ src/views/EventMainView.vue
@@ -1,7 +1,26 @@
 <template>
-  <div></div>
+  <FormPageLayout>
+    <template v-if="previewCards.length > 0">
+      <template v-for="(card, id) in previewCards" :key="id">
+        <EventPreviewCard :card="card" />
+      </template>
+    </template>
+  </FormPageLayout>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import FormPageLayout from '@/components/layouts/FormPageLayout.vue';
+import EventPreviewCard from '@/components/cards/preview/EventPreviewCard.vue';
+
+const previewCards = ref([
+  { title: 'hi' },
+  { title: 'hi' },
+  { title: 'hi' },
+  { title: 'hi' },
+  { title: 'hi' },
+  { title: 'hi' },
+]);
+</script>
 
 <style scoped></style>
