dba3925c1c26917bcf76702f2dd6ffc0a8aaa864
yanghyeonjin <guswls7081@gmail.com>
2024-10-14 16:44:31 +0900

    feat: 리뷰 작성 페이지 UI 완료 #17

---
+++ src/components/form/UploadImage.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="upload-image">
+    <label for="">이미지 업로드</label>
+    <span class="helper-text">최대 {{ maxCnt }}장까지 가능</span>
+    <Button label="파일 선택" severity="secondary" @click="clickUpload" />
+    <ul v-if="previewUrls.length > 0">
+      <li v-for="(url, index) in previewUrls" :key="url">
+        <Image :src="url" width="150" height="150" preview image-style="object-fit: cover;" />
+        <Button
+          icon="pi pi-times"
+          severity="secondary"
+          rounded
+          raised
+          aria-label="삭제"
+          class="btn-remove"
+          @click="clickRemove(index)"
+        />
+      </li>
+    </ul>
+    <input ref="refInputFile" type="file" multiple accept="image/*" style="display: none" @change="changeFile" />
+  </div>
+</template>
+
+<script setup>
+import { onUnmounted, ref } from 'vue';
+import Button from 'primevue/button';
+import Image from 'primevue/image';
+import { defineProps } from 'vue';
+import useToastMessage from '@/hooks/useToastMessage';
+
+const props = defineProps({
+  currentCnt: {
+    type: Number,
+    required: true,
+  },
+  currentFiles: {
+    type: Array,
+    required: true,
+  },
+});
+
+// onChangeFiles: 생성, 삭제가 완료된(처리가 완료된) 전체 이미지 파일 배열을 넘겨주기 위해 사용
+const emit = defineEmits(['onChangeFiles']);
+
+const { showWarning } = useToastMessage();
+
+const maxCnt = ref(5);
+const previewUrls = ref([]);
+const refInputFile = ref(null);
+
+const clickUpload = () => {
+  refInputFile.value.click();
+};
+
+const changeFile = event => {
+  const {
+    target: { files },
+  } = event;
+  const length = files.length;
+  const newFiles = [...props.currentFiles];
+  const newPreviewUrls = [...previewUrls.value];
+
+  if (props.currentCnt + length > maxCnt.value) {
+    showWarning('최대 개수 초과', `최대 ${maxCnt.value}장까지 업로드 가능합니다.`);
+    return;
+  }
+
+  for (let i = 0; i < length; i++) {
+    const file = files[i];
+    newFiles.push(file);
+    newPreviewUrls.push(URL.createObjectURL(file)); // 프리뷰 이미지 생성
+  }
+
+  emit('onChangeFiles', newFiles);
+  previewUrls.value = newPreviewUrls;
+};
+
+const clickRemove = targetIndex => {
+  // 프리뷰 이미지 삭제
+  previewUrls.value = previewUrls.value.filter((url, index) => {
+    if (index === targetIndex) {
+      URL.revokeObjectURL(url);
+    } else {
+      return url;
+    }
+  });
+
+  // 파일 삭제
+  const newFiles = [...props.currentFiles].filter((file, index) => index !== targetIndex);
+  emit('onChangeFiles', newFiles);
+};
+
+onUnmounted(() => {
+  // 프리뷰 이미지 객체 할당 해제
+  previewUrls.value.forEach(url => {
+    URL.revokeObjectURL(url);
+  });
+});
+</script>
+
+<style scoped>
+.upload-image {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  .helper-text {
+    color: #757575;
+    font-size: 14px;
+    margin-top: 4px;
+    margin-bottom: 10px;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    margin-top: 16px;
+    display: flex;
+    gap: 24px;
+
+    li {
+      position: relative;
+    }
+
+    .btn-remove {
+      position: absolute;
+      top: -16px;
+      right: -16px;
+    }
+  }
+}
+</style>
--- src/views/theme/ThemeReviewFormView.vue
+++ src/views/theme/ThemeReviewFormView.vue
@@ -1,63 +1,99 @@
 <template>
   <FormPageLayout title="리뷰 작성">
-    <div class="review-form-container">
-      <Panel header="참여한 테마">
-        <div>{{ themeName }}</div>
-      </Panel>
+    <div class="mb-xl">테마명: {{ themeName }}</div>
 
-      <div class="flex-row gap-10">
-        <div>
-          <h5>참가 인원</h5>
-          <InputNumber v-model="peopleNum" suffix="명" />
-        </div>
-
-        <div>
-          <h5>소요 시간</h5>
-          <InputNumber v-model="time" suffix="분" />
-        </div>
-      </div>
-
-      <div>
-        <h5>문제 구성</h5>
-        <SelectButton v-model="quizQuality" :options="quizQualityOptions" aria-labelledby="문제 구성" />
-      </div>
-
-      <div>
-        <h5>체감 난이도</h5>
-        <SelectButton v-model="level" :options="levelOptions" aria-labelledby="체감 난이도" />
-      </div>
-
-      <div>
-        <h5>체감 공포도</h5>
-        <SelectButton v-model="scary" :options="scaryOptions" aria-labelledby="체감 공포도" />
-      </div>
-
-      <div>
-        <h5>체감 활동성</h5>
-        <SelectButton v-model="activity" :options="activityOptions" aria-labelledby="체감 활동성" />
-      </div>
-
-      <div>
-        <h5>인테리어</h5>
-        <SelectButton v-model="interior" :options="interiorOptions" aria-labelledby="인테리어" />
-      </div>
-
-      <div>
-        <h5>개연성</h5>
-        <SelectButton v-model="probability" :options="probabilityOptions" aria-labelledby="개연성" />
-      </div>
-
-      <div>
-        <h5>리뷰 내용</h5>
-        <Textarea
-          v-model="reviewContent"
-          auto-resize
-          rows="6"
-          cols="100"
-          placeholder="개성 넘치는 리뷰를 작성해주세요!"
-        />
-      </div>
+    <div class="flex flex-col items-center mb-xl">
+      <h3 class="mb-s">이번 방탈출은 어떠셨나요? 만족도를 평가해주세요!</h3>
+      <Rating v-model="totalScore">
+        <template #onicon>
+          <img src="https://primefaces.org/cdn/primevue/images/rating/custom-onicon.png" height="32" width="32" />
+        </template>
+        <template #officon>
+          <img src="https://primefaces.org/cdn/primevue/images/rating/custom-officon.png" height="32" width="32" />
+        </template>
+      </Rating>
     </div>
+
+    <div class="flex-col items-center gap-20 mb-xl">
+      <div class="flex-row gap-10">
+        <FloatLabel variant="in">
+          <InputNumber id="peopleNum" v-model="peopleNum" suffix="명" />
+          <label for="peopleNum">참가 인원</label>
+        </FloatLabel>
+
+        <FloatLabel variant="in">
+          <InputNumber id="time" v-model="time" suffix="분" />
+          <label for="time">소요 시간</label>
+        </FloatLabel>
+      </div>
+
+      <SelectButtonWithLabel
+        :selected="quizQuality"
+        :options-ko="[
+          '문제가 엉성해요',
+          '문제가 별로예요',
+          '보통이에요',
+          '잘 구성되어 있어요',
+          '매우 잘 구성되어 있어요',
+        ]"
+        label="문제 구성"
+        @change-value="onChangeQuizQuality"
+      />
+
+      <SelectButtonWithLabel
+        :selected="level"
+        :options-ko="['아주 쉬워요', '쉬워요', '보통이에요', '어려워요', '아주 어려워요']"
+        label="체감 난이도"
+        @change-value="onChangeLevel"
+      />
+
+      <SelectButtonWithLabel
+        :selected="scary"
+        :options-ko="['하나도 안무서워요', '안무서워요', '보통이에요', '무서워요', '아주 무서워요']"
+        label="체감 공포도"
+        @change-value="onChangeScary"
+      />
+
+      <SelectButtonWithLabel
+        :selected="activity"
+        :options-ko="[
+          '전혀 활동적이지 않아요',
+          '활동이 거의 없어요',
+          '보통이에요',
+          '활동적이에요',
+          '매우 활동적이에요',
+        ]"
+        label="체감 활동성"
+        @change-value="onChangeActivity"
+      />
+
+      <SelectButtonWithLabel
+        :selected="interior"
+        :options-ko="['전혀 신경 안 쓴 인테리어예요', '아쉬워요', '보통이에요', '신경을 많이 썼어요', '매우 훌륭해요']"
+        label="인테리어"
+        @change-value="onChangeInterior"
+      />
+
+      <SelectButtonWithLabel
+        :selected="probability"
+        :options-ko="[
+          '전혀 개연성이 없어요',
+          '개연성이 아쉬워요',
+          '보통이에요',
+          '개연성이 있어요',
+          '매우 개연성 있어요',
+        ]"
+        label="개연성"
+        @change-value="onChangeProbability"
+      />
+    </div>
+
+    <FloatLabel class="mb-xl">
+      <Textarea id="reviewContent" v-model="reviewContent" auto-resize rows="6" cols="100" fluid />
+      <label for="reviewContent">리뷰 내용</label>
+    </FloatLabel>
+
+    <UploadImage :current-files="currentFiles" :current-cnt="currentFiles.length" @on-change-files="onChangeFiles" />
   </FormPageLayout>
 </template>
 
@@ -65,53 +101,55 @@
 import FormPageLayout from '@/components/layouts/FormPageLayout.vue';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
-import Panel from 'primevue/panel';
 import InputNumber from 'primevue/inputnumber';
-import SelectButton from 'primevue/selectbutton';
+import SelectButtonWithLabel from '@/components/form/SelectButtonWithLabel.vue';
+import FloatLabel from 'primevue/floatlabel';
+import UploadImage from '@/components/form/UploadImage.vue';
+import Rating from 'primevue/rating';
 
 const route = useRoute();
 const { themeId } = route.params;
 
 const themeName = ref('');
+const totalScore = ref(null);
 const peopleNum = ref(null);
 const time = ref(null);
-const quizQuality = ref('');
-const quizQualityOptions = ref([
-  '문제가 엉성해요',
-  '문제가 별로예요',
-  '보통이에요',
-  '잘 구성되어 있어요',
-  '매우 잘 구성되어 있어요',
-]);
-const level = ref('');
-const levelOptions = ref(['아주 쉬워요', '쉬워요', '보통이에요', '어려워요', '아주 어려워요']);
-const scary = ref('');
-const scaryOptions = ref(['하나도 안무서워요', '안무서워요', '보통이에요', '무서워요', '아주 무서워요']);
-const activity = ref('');
-const activityOptions = ref([
-  '전혀 활동적이지 않아요',
-  '활동이 거의 없어요',
-  '보통이에요',
-  '활동적이에요',
-  '매우 활동적이에요',
-]);
-const interior = ref('');
-const interiorOptions = ref([
-  '전혀 신경 안 쓴 인테리어예요',
-  '아쉬워요',
-  '보통이에요',
-  '신경을 많이 썼어요',
-  '매우 훌륭해요',
-]);
-const probability = ref('');
-const probabilityOptions = ref([
-  '전혀 개연성이 없어요',
-  '개연성이 아쉬워요',
-  '보통이에요',
-  '개연성이 있어요',
-  '매우 개연성 있어요',
-]);
+const quizQuality = ref({});
+const level = ref({});
+const scary = ref({});
+const activity = ref({});
+const interior = ref({});
+const probability = ref({});
 const reviewContent = ref('');
+const currentFiles = ref([]);
+
+const onChangeQuizQuality = newVal => {
+  quizQuality.value = newVal;
+};
+
+const onChangeLevel = newVal => {
+  level.value = newVal;
+};
+
+const onChangeScary = newVal => {
+  scary.value = newVal;
+};
+
+const onChangeActivity = newVal => {
+  activity.value = newVal;
+};
+
+const onChangeInterior = newVal => {
+  interior.value = newVal;
+};
+
+const onChangeProbability = newVal => {
+  probability.value = newVal;
+};
+
+const onChangeFiles = newFiles => {
+  currentFiles.value = newFiles;
+};
 
 onMounted(() => {
   // TODO:: themeId 테마 정보 조회
@@ -119,10 +157,4 @@ onMounted(() => {
 });
 </script>
 
-<style scoped>
-.review-form-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-</style>
+<style scoped></style>
