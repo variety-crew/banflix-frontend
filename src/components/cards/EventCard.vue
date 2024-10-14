ea185dc689b7cdc3440bf506e39819d6d72d37f6
gilhan <rlfgks97@gmail.com>
2024-10-14 16:47:05 +0900

    feat: 이벤트 카드 컴포넌트 추가

---
+++ src/components/cards/EventCard.vue
@@ -0,0 +1,91 @@
+<template>
+  <Card @click="goEventDetail(card.id)">
+    <template #content>
+      <div class="content-container">
+        <img class="theme-tumb" :src="props.card.profileImage" alt="테마 프로필 이미지" />
+        <h3 class="theme">{{ props.card.theme }}</h3>
+        <div class="partner">{{ props.card.partner }}</div>
+        <div class="location">{{ props.card.location }}</div>
+        <div class="reaction-container">
+          <Like :like="props.card.reaction.like" />
+          <Like :like="props.card.reaction.like" />
+          <Like :like="props.card.reaction.like" />
+        </div>
+      </div>
+    </template>
+  </Card>
+</template>
+
+<script setup>
+import { ref, defineProps } from 'vue';
+import Card from 'primevue/card';
+import Image from 'primevue/image';
+import Like from '../common/reaction/Like.vue';
+const props = defineProps({
+  card: {
+    type: Object,
+    required: true,
+  },
+  // card object
+  // {
+  //   id: 2,
+  //   profileImage: 'https://github.com/user-attachments/assets/04e68ff8-44ad-4b43-b5f0-9fa1c6704842',
+  //   theme: '위험한 동굴3',
+  //   partner: '그레이트 이스케이프 신촌점',
+  //   location: '서울 동작구 여의대방로 188-13',
+  //   reaction: {
+  //     reviewCount: 7,
+  //     like: {
+  //       liked: false,
+  //       count: 8,
+  //     },
+  //     scrap: {
+  //       scrapped: true,
+  //       scrapped: 9,
+  //     },
+  //   },
+  // },
+  isLoggined: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const goEventDetail = id => {
+  console.log(id);
+};
+</script>
+
+<style scoped>
+.p-card {
+  width: 300px;
+}
+
+.content-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.theme-tumb {
+  margin: 0 auto;
+  /* align-self: center; */
+  width: 200px;
+  height: 200px;
+}
+.theme {
+  margin-top: 20px;
+  font-weight: bolder;
+}
+.partner {
+  margin-top: 10px;
+}
+.location {
+  margin-top: 10px;
+}
+
+.reaction-container {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
+}
+</style>
--- src/views/EventMainView.vue
+++ src/views/EventMainView.vue
@@ -1,31 +1,111 @@
 <template>
   <PageLayout title="이벤트" btn-txt="글 작성" btn-role="ADMIN" @click-btn="goEventForm">
-    <template v-if="previewCards.length > 0">
-      <template v-for="(card, id) in previewCards" :key="id">
-        <EventPreviewCard :card="card" />
-      </template>
+    <h3>할인 테마</h3>
+
+    <template v-if="cards.length > 0">
+      <div class="event-card-container">
+        <template v-for="(card, id) in cards" :key="id">
+          <EventCard :card="card" />
+        </template>
+      </div>
     </template>
+    <template v-else> 이벤트가 존재하지 않습니다. </template>
   </PageLayout>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 import PageLayout from '@/components/layouts/PageLayout.vue';
-import EventPreviewCard from '@/components/cards/preview/EventPreviewCard.vue';
+import EventCard from '@/components/cards/EventCard.vue';
 import router from '@/router/routes';
 
 const goEventForm = () => {
   router.push(`/form/event`);
 };
 
-const previewCards = ref([
-  { title: 'hi' },
-  { title: 'hi' },
-  { title: 'hi' },
-  { title: 'hi' },
-  { title: 'hi' },
-  { title: 'hi' },
+const goEventDetail = id => {
+  router.push(`/detail/event/${id}`);
+};
+
+const cards = ref([
+  {
+    id: 0,
+    profileImage: 'https://github.com/user-attachments/assets/04e68ff8-44ad-4b43-b5f0-9fa1c6704842',
+    theme: '위험한 동굴1',
+    partner: '그레이트 이스케이프 신촌점',
+    location: '서울 동작구 여의대방로 188-11',
+    reaction: {
+      reviewCount: 1,
+      like: {
+        liked: false,
+        count: 2,
+      },
+      scrap: {
+        scrapped: true,
+        scrapped: 3,
+      },
+    },
+  },
+  {
+    id: 1,
+    profileImage: 'https://github.com/user-attachments/assets/04e68ff8-44ad-4b43-b5f0-9fa1c6704842',
+    theme: '위험한 동굴2',
+    partner: '그레이트 이스케이프 신촌점',
+    location: '서울 동작구 여의대방로 188-12',
+    reaction: {
+      reviewCount: 4,
+      like: {
+        liked: false,
+        count: 5,
+      },
+      scrap: {
+        scrapped: true,
+        scrapped: 6,
+      },
+    },
+  },
+  {
+    id: 2,
+    profileImage: 'https://github.com/user-attachments/assets/04e68ff8-44ad-4b43-b5f0-9fa1c6704842',
+    theme: '위험한 동굴3',
+    partner: '그레이트 이스케이프 신촌점',
+    location: '서울 동작구 여의대방로 188-13',
+    reaction: {
+      reviewCount: 7,
+      like: {
+        liked: true,
+        count: 8,
+      },
+      scrap: {
+        scrapped: true,
+        scrapped: 9,
+      },
+    },
+  },
+  {
+    id: 3,
+    profileImage: 'https://github.com/user-attachments/assets/04e68ff8-44ad-4b43-b5f0-9fa1c6704842',
+    theme: '위험한 동굴4',
+    partner: '그레이트 이스케이프 신촌점',
+    location: '서울 동작구 여의대방로 188-14',
+    reaction: {
+      reviewCount: 10,
+      like: {
+        liked: false,
+        count: 11,
+      },
+      scrap: {
+        scrapped: true,
+        scrapped: 12,
+      },
+    },
+  },
 ]);
 </script>
 
-<style scoped></style>
+<style scoped>
+.event-card-container {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
