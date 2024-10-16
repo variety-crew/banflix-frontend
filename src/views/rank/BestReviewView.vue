58c957eb4b9f6ff6126127ec5804fd14bfc634fd
yanghyeonjin <guswls7081@gmail.com>
2024-10-16 16:37:16 +0900

    feat: 전체 베스트 리뷰 화면 UI 완료 #35

---
+++ src/views/rank/BestReviewView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex-col gap-20">
+    <ReviewItem v-for="review in props.reviews" :key="review.id" :review="review" show-theme />
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+import ReviewItem from '@/components/review/ReviewItem.vue';
+
+const props = defineProps({
+  reviews: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<style scoped></style>
