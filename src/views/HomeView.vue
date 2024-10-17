9d8d40570affb4900ea50612f9027c3e66a510e5
yanghyeonjin <guswls7081@gmail.com>
2024-10-18 01:25:07 +0900

    feat: 메인 페이지 개발중 #48

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,26 @@
 <template>
-  <div></div>
+  <PageLayout title="방플릭스!">
+    <HomeViewThemeList section-title="이번 주 TOP5 테마를 확인해 보세요" :themes="[]" />
+    <HomeViewThemeList :section-title="`${userStore.nickname}님의 취향과 일치하는 테마예요`" :themes="[]" />
+    <HomeViewThemeList section-title="미스터리" :themes="[]" />
+    <HomeViewThemeList section-title="범죄" :themes="[]" />
+  </PageLayout>
 </template>
 
-<script setup></script>
+<script setup>
+import HomeViewThemeList from '@/components/HomeViewThemeList.vue';
+import PageLayout from '@/components/layouts/PageLayout.vue';
+import { $api } from '@/services/api/api';
+import { useUserStore } from '@/stores/user';
+import { onMounted } from 'vue';
+
+const userStore = useUserStore();
+
+// 메인 페이지 데이터 조회
+onMounted(async () => {
+  // 1. 전체 장르 목록 조회
+  const genres = await $api.theme.get('genres');
+});
+</script>
 
 <style scoped></style>
