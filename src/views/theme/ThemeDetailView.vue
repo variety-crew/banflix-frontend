e314d2d1c08733445752953113ad277724dff5b9
yanghyeonjin <guswls7081@gmail.com>
2024-10-18 16:46:55 +0900

    feat: 리뷰 통계 API 완료

--- src/views/theme/ThemeDetailView.vue
+++ src/views/theme/ThemeDetailView.vue
@@ -53,29 +53,32 @@
     </div>
 
     <!-- 리뷰 통계 -->
-    <Panel header="" class="area-review-statistics mb-l">
-      <div class="top">
-        <div>
-          <h3 class="mb-s">평균 만족도</h3>
-          <h1>4.11 / 5</h1>
+    <Fieldset legend="리뷰 통계" class="area-review-statistics">
+      <div v-if="!reviewStatistics">아직 리뷰가 없어요! 리뷰를 작성해주세요</div>
+      <div v-else>
+        <div class="top">
+          <div>
+            <h3 class="mb-s">평균 만족도</h3>
+            <h1>{{ reviewStatistics.avgTotalScore }} / 5</h1>
+          </div>
+          <Divider layout="vertical" />
+          <div>
+            <ReviewStatisticsItem :data="totalScoreStatisticsData" />
+          </div>
         </div>
-        <Divider layout="vertical" />
-        <div>
-          <ReviewStatisticsItem :data="totalScoreStatisticsData" />
+
+        <Divider />
+
+        <div class="bottom">
+          <ReviewStatisticsItem label="문제구성" :data="quizQualityStatisticsData" />
+          <ReviewStatisticsItem label="난이도" :data="levelStatisticsData" />
+          <ReviewStatisticsItem label="공포도" :data="scaryStatisticsData" />
+          <ReviewStatisticsItem label="활동성" :data="activityStatisticsData" />
+          <ReviewStatisticsItem label="인테리어" :data="interiorStatisticsData" />
+          <ReviewStatisticsItem label="개연성" :data="probabilityStatisticsData" />
         </div>
       </div>
-
-      <Divider />
-
-      <div class="bottom">
-        <ReviewStatisticsItem label="문제구성" :data="quizQualityStatisticsData" />
-        <ReviewStatisticsItem label="난이도" :data="levelStatisticsData" />
-        <ReviewStatisticsItem label="공포도" :data="scaryStatisticsData" />
-        <ReviewStatisticsItem label="활동성" :data="activityStatisticsData" />
-        <ReviewStatisticsItem label="인테리어" :data="interiorStatisticsData" />
-        <ReviewStatisticsItem label="개연성" :data="probabilityStatisticsData" />
-      </div>
-    </Panel>
+    </Fieldset>
 
     <!-- 리뷰 -->
     <section>
@@ -122,6 +125,7 @@ import ReviewList from '@/components/review/ReviewList.vue';
 import { $api } from '@/services/api/api';
 import PageLoadingLayout from '@/components/layouts/PageLoadingLayout.vue';
 import Image from 'primevue/image';
+import Fieldset from 'primevue/fieldset';
 
 const router = useRouter();
 const route = useRoute();
@@ -129,23 +133,23 @@ const { themeId } = route.params;
 
 const themeDetail = ref(null);
 const totalScoreStatisticsData = ref([
-  { label: '1점', percent: 1 },
+  { label: '1점', value: 'ONE', percent: 1 },
   { label: '2점', percent: 2 },
   { label: '3점', percent: 10 },
   { label: '4점', percent: 25 },
   { label: '5점', percent: 62 },
 ]);
-const quizQualityStatisticsData = ref(quizQualityOptions.map((e, i) => ({ ...e, percent: 10 * (i + 1) })));
-const levelStatisticsData = ref(levelOptions.map((e, i) => ({ ...e, percent: 10 * (i + 1) })));
-const scaryStatisticsData = ref(scaryOptions.map((e, i) => ({ ...e, percent: 10 * (i + 1) })));
-const activityStatisticsData = ref(activityOptions.map((e, i) => ({ ...e, percent: 10 * (i + 1) })));
-const interiorStatisticsData = ref(interiorOptions.map((e, i) => ({ ...e, percent: 10 * (i + 1) })));
-const probabilityStatisticsData = ref(probabilityOptions.map((e, i) => ({ ...e, percent: 10 * (i + 1) })));
-const selectedReviewSorting = ref({ name: '최신 순', value: 'RECENT' });
+const quizQualityStatisticsData = ref([]);
+const levelStatisticsData = ref([]);
+const scaryStatisticsData = ref([]);
+const activityStatisticsData = ref([]);
+const interiorStatisticsData = ref([]);
+const probabilityStatisticsData = ref([]);
+const selectedReviewSorting = ref({ name: '최신 순', value: '' });
 const reviewSortingOptions = ref([
-  { name: '최신 순', value: 'RECENT' },
-  { name: '만족도 높은 순', value: 'SCORE_DESC' },
-  { name: '만족도 낮은 순', value: 'SCORE_ASC' },
+  { name: '최신 순', value: '' },
+  { name: '만족도 높은 순', value: 'highScore' },
+  { name: '만족도 낮은 순', value: 'lowScore' },
 ]);
 const reviews = ref([
   {
@@ -194,6 +198,15 @@ const reviews = ref([
 ]);
 const userLiked = ref(true);
 const userBookmarked = ref(false);
+const reviewStatistics = ref(null);
+
+const totalScoreOptions = [
+  { label: '1점', value: 'ONE' },
+  { label: '2점', value: 'TWO' },
+  { label: '3점', value: 'THREE' },
+  { label: '4점', value: 'FOUR' },
+  { label: '5점', value: 'FIVE' },
+];
 
 const goReviewForm = themeCode => {
   router.push(`/theme/${themeCode}/create-review`);
@@ -211,6 +224,25 @@ const clickStore = storeCode => {
   router.push(`/store/detail/${storeCode}`);
 };
 
+const mappingStatistics = (options, one, two, three, four, five) => {
+  return options.map(e => {
+    let percent = 0;
+
+    if (e.value === 'ONE') {
+      percent = one;
+    } else if (e.value === 'TWO') {
+      percent = two;
+    } else if (e.value === 'THREE') {
+      percent = three;
+    } else if (e.value === 'FOUR') {
+      percent = four;
+    } else if (e.value === 'FIVE') {
+      percent = five;
+    }
+    return { ...e, percent };
+  });
+};
+
 // 테마코드로 조회
 watchEffect(() => {
   if (!themeId) return;
@@ -221,7 +253,122 @@ watchEffect(() => {
   });
 
   // 리뷰 통계 조회
-  $api.review.getStatisticsByThemeCode(themeId).then(statistics => {});
+  $api.review.getStatisticsByThemeCode(themeId).then(statistics => {
+    reviewStatistics.value = statistics;
+
+    const {
+      oneCompositionPercent,
+      twoCompositionPercent,
+      threeCompositionPercent,
+      fourCompositionPercent,
+      fiveCompositionPercent,
+
+      oneLevelPercent,
+      twoLevelPercent,
+      threeLevelPercent,
+      fourLevelPercent,
+      fiveLevelPercent,
+
+      oneHorrorLevelPercent,
+      twoHorrorLevelPercent,
+      threeHorrorLevelPercent,
+      fourHorrorLevelPercent,
+      fiveHorrorLevelPercent,
+
+      oneActivePercent,
+      twoActivePercent,
+      threeActivePercent,
+      fourActivePercent,
+      fiveActivePercent,
+
+      oneInteriorPercent,
+      twoInteriorPercent,
+      threeInteriorPercent,
+      fourInteriorPercent,
+      fiveInteriorPercent,
+
+      oneProbabilityPercent,
+      twoProbabilityPercent,
+      threeProbabilityPercent,
+      fourProbabilityPercent,
+      fiveProbabilityPercent,
+
+      oneScorePercent,
+      twoScorePercent,
+      threeScorePercent,
+      fourScorePercent,
+      fiveScorePercent,
+    } = statistics;
+
+    // 문제구성
+    quizQualityStatisticsData.value = mappingStatistics(
+      quizQualityOptions,
+      oneCompositionPercent,
+      twoCompositionPercent,
+      threeCompositionPercent,
+      fourCompositionPercent,
+      fiveCompositionPercent,
+    );
+
+    // 난이도
+    levelStatisticsData.value = mappingStatistics(
+      levelOptions,
+      oneLevelPercent,
+      twoLevelPercent,
+      threeLevelPercent,
+      fourLevelPercent,
+      fiveLevelPercent,
+    );
+
+    // 공포도
+    scaryStatisticsData.value = mappingStatistics(
+      scaryOptions,
+      oneHorrorLevelPercent,
+      twoHorrorLevelPercent,
+      threeHorrorLevelPercent,
+      fourHorrorLevelPercent,
+      fiveHorrorLevelPercent,
+    );
+
+    // 활동성
+    activityStatisticsData.value = mappingStatistics(
+      activityOptions,
+      oneActivePercent,
+      twoActivePercent,
+      threeActivePercent,
+      fourActivePercent,
+      fiveActivePercent,
+    );
+
+    // 인테리어
+    interiorStatisticsData.value = mappingStatistics(
+      interiorOptions,
+      oneInteriorPercent,
+      twoInteriorPercent,
+      threeInteriorPercent,
+      fourInteriorPercent,
+      fiveInteriorPercent,
+    );
+
+    // 개연성
+    probabilityStatisticsData.value = mappingStatistics(
+      probabilityOptions,
+      oneProbabilityPercent,
+      twoProbabilityPercent,
+      threeProbabilityPercent,
+      fourProbabilityPercent,
+      fiveProbabilityPercent,
+    );
+
+    totalScoreStatisticsData.value = mappingStatistics(
+      totalScoreOptions,
+      oneScorePercent,
+      twoScorePercent,
+      threeScorePercent,
+      fourScorePercent,
+      fiveScorePercent,
+    );
+  });
 
   // 리뷰 목록 조회
   $api.review.getReviewsByThemeCode(themeId).then(reviews => {});
@@ -234,6 +381,7 @@ watchEffect(() => {
     display: flex;
     flex-direction: column;
     flex-grow: 1;
+    margin-bottom: 32px;
 
     .top {
       display: grid;
