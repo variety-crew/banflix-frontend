36f2980b295ecf1c57b9ee25424dbbc236c18f59
hoddeok07 <jhj2969@naver.com>
2024-10-20 11:55:14 +0900

    feat: 커뮤니티 게시글 목록 조회 api 연동

---
+++ src/services/api/CommunityLikeApiService.js
@@ -0,0 +1,11 @@
+import ApiService from '@/services/api/config/ApiService';
+
+export default class CommunityLikeApiService extends ApiService {
+  constructor() {
+    super('v1/community-like');
+  }
+
+  getLikeCount(communityPostCode) {
+    return this.get(`${communityPostCode}`);
+  }
+}
